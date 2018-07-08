2e885d4c2c8cd40452611c1c37efa5ffc4e04eb4
Navjot <navjot.singh2472017@gmail.com>
2018-07-08 10:25:24 +0530

    first commit

---
+++ app/src/main/java/com/farehawker/farehawker/Icon.java
@@ -0,0 +1,18 @@
+package com.farehawker.farehawker;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Waheguru on 7/8/2018.
+ */
+
+public class Icon
+{
+    private ImageView icon_image;
+    private TextView icon_text;
+    public Icon()
+    {
+
+    }
+}
